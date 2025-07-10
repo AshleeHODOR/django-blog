83b97288edc2aac5eb31da4f42887261465a3481
Ashlee Hodor <ashlee.hodor@SDGKU.edu>
2025-07-09 20:04:59 -0700

    updated blog

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import AboutPageView, HomePageView
+#from pages import views (different way)
+#from pages import views 
 
 urlpatterns = [
         path("", HomePageView.as_view(), name="home"),
