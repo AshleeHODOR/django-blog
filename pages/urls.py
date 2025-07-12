2b6bbb5822e202f0c455026d488fbc0062e1cb7f
Ashlee Hodor <ashlee.hodor@SDGKU.edu>
2025-07-11 18:02:32 -0700

    Completed Homework_blog added

--- pages/urls.py
+++ pages/urls.py
@@ -5,5 +5,5 @@ from .views import AboutPageView, HomePageView
 
 urlpatterns = [
         path("", HomePageView.as_view(), name="home"),
-        path("about/", AboutPageView.as_view(), name="about")
+        path("about/", AboutPageView.as_view(), name="about"), 
     ]
